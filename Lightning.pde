fde9bedb70160ce79e0cd32f5755d7cec35258e0
unknown <ANXU4@697-334-Aragorn.sf.k12.internal>
2015-09-02 08:00:24 -0700

    Made thunder

--- Lightning.pde
+++ Lightning.pde
@@ -1,21 +1,28 @@
-int startX=150;
+int startX=400;
 int startY=0;
-int endX=150;
+int endX=400;
 int endY=0;
+int blazeIt=420;
 void setup()
 {
-  size(300,300);
+  size(800,800);
   background(0);
   strokeWeight(3);
-  frameRate(200);
+  frameRate(200);  
 }
 void draw()
+{
+	Lightning();
+	Cloud();
+	Flash();
+}
+void Lightning()
 {
 	float r=(int)(Math.random()*256);
 	float g=(int)(Math.random()*256);
 	float b=(int)(Math.random()*256);
 	stroke(r,g,b);
-	while(endY<300)
+	while(endY<800)
 	{
 		endX=startX+(int)(Math.random()*20)-10;
 		endY=startY+(int)(Math.random()*10);
@@ -25,11 +32,38 @@ void draw()
 	}
 
 }
+void Cloud()
+{
+	fill(200);
+	ellipse(400,0,600,100);
+}
+void Flash()
+{
+	if(blazeIt<420)
+	{
+		blazeIt++;
+	}
+	if(blazeIt>400)
+	{
+		blazeIt++;
+		stroke(0);
+		fill(0,255,255);
+		rect(0,0,800,800);
+	}
+	if(blazeIt>420)
+	{
+		stroke(0);
+		fill(0);
+		rect(0,0,800,800);
+		blazeIt=0;
+	}
+
+}
 void mousePressed()
 {
-	startX=150;
+	startX=(int)(Math.random()*400)+200;
 	startY=0;
-	endX=150;
+	endX=(int)(Math.random()*400)+200;
 	endY=0;
 }
 
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,24 +14,31 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
-int startX=150;
+int startX=400;
 int startY=0;
-int endX=150;
+int endX=400;
 int endY=0;
+int blazeIt=420;
 public void setup()
 {
-  size(300,300);
+  size(800,800);
   background(0);
   strokeWeight(3);
-  frameRate(200);
+  frameRate(200);  
 }
 public void draw()
+{
+	Lightning();
+	Cloud();
+	Flash();
+}
+public void Lightning()
 {
 	float r=(int)(Math.random()*256);
 	float g=(int)(Math.random()*256);
 	float b=(int)(Math.random()*256);
 	stroke(r,g,b);
-	while(endY<300)
+	while(endY<800)
 	{
 		endX=startX+(int)(Math.random()*20)-10;
 		endY=startY+(int)(Math.random()*10);
@@ -41,11 +48,38 @@ public void draw()
 	}
 
 }
+public void Cloud()
+{
+	fill(200);
+	ellipse(400,0,600,100);
+}
+public void Flash()
+{
+	if(blazeIt<420)
+	{
+		blazeIt++;
+	}
+	if(blazeIt>400)
+	{
+		blazeIt++;
+		stroke(0);
+		fill(0,255,255);
+		rect(0,0,800,800);
+	}
+	if(blazeIt>420)
+	{
+		stroke(0);
+		fill(0);
+		rect(0,0,800,800);
+		blazeIt=0;
+	}
+
+}
 public void mousePressed()
 {
-	startX=150;
+	startX=(int)(Math.random()*400)+200;
 	startY=0;
-	endX=150;
+	endX=(int)(Math.random()*400)+200;
 	endY=0;
 }
 
